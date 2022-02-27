5c77b203566fe5effa2854f796dd942623949dd9
pankowskitomasz <pankowski.email@gmail.com>
2022-02-27 16:55:34 +0200

    routing

---
+++ src/views/home.js
@@ -0,0 +1,11 @@
+import {Container} from "react-bootstrap";
+
+function Home(){
+    return(
+        <Container fluid className="minh-footer-adj bg-dark d-flex align-items-center py-5">
+            
+        </Container>
+    );
+}
+
+export default Home;
---
+++ src/views/tours.js
@@ -0,0 +1,11 @@
+import {Container} from "react-bootstrap";
+
+function Tours(){
+    return(
+        <Container fluid className="minh-footer-adj bg-dark d-flex align-items-center py-5">
+            
+        </Container>
+    );
+}
+
+export default Tours;
