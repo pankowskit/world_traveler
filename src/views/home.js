664621d253f7594c85a2fb97cc4d5238b6426da9
pankowskit <dagi@10g.pl>
2022-06-27 11:52:06 +0200

    layout

--- src/views/home.js
+++ src/views/home.js
@@ -1,11 +1,15 @@
-import {Container} from "react-bootstrap";
+import React,{Component} from "react";
+import Container from "../../node_modules/react-bootstrap/Container";
+import IndexS1 from "../components/index-s1"
 
-function Home(){
-    return(
-        <Container fluid className="minh-footer-adj bg-dark d-flex align-items-center py-5">
-            
-        </Container>
-    );
+class Home extends Component{
+    render(){
+        return(        
+            <Container fluid className="d-flex align-items-center minh-footer-adj p-0">
+                <IndexS1/>
+            </Container>    
+        );
+    }
 }
 
 export default Home;
